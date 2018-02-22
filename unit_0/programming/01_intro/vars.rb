0beb29702094d95dfdba28dbd8d6f30fce2e0b54
Kyle <kyle1james@gmail.com>
2018-02-22 07:54:12 -0500

    update unit_0

--- unit_0/programming/01_intro/vars.rb
+++ unit_0/programming/01_intro/vars.rb
@@ -1,1 +1,20 @@
-vars.rb
+# Data Types str, int, bool
+# String interpolation 
+# Input Output
+# If/else
+
+puts 'what is your name'
+hello = gets.chomp
+puts "Hello, #{hello}"
+
+puts 'how old are you?'
+age = gets.chomp
+puts age + ' ,wow, that is old'
+
+if age.to_i > 20
+	puts 'yikes'
+elsif age.to_i. > 30
+	puts ' double yikes'
+else
+	puts 'oh'
+end
