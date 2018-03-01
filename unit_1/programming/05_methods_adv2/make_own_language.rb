91829923c08163e834108266e43d26ed79c8c31b
Kyle <kyle1james@gmail.com>
2018-03-01 08:03:32 -0500

    moved adv methods2

--- unit_1/programming/05_methods_adv2/make_own_language.rb
+++ unit_1/programming/05_methods_adv2/make_own_language.rb
@@ -1,8 +1,8 @@
 class Encryptor
-
+# Have class annotate entire class tomorrow
 	def cipher
 		{'a' => '0110001', 'b' => '01100010', 'c' => '01000011', 
-		'd' => '01100100', 'e' => '01000101', 'e' => '01100101', 
+		'd' => '01100100', 'e' => '01000101',  
 		'f' => '01100110', 'g' => '01100111', 'h' => '01101000', 
 		'i' => '01101001', 'j' => '01101010', 'k' => '01101011',
 		'l' => '01101100', 'm' => '01101101', 'n' => '01101110', 
@@ -29,5 +29,23 @@ class Encryptor
   		end
 
   	results.join
+	end
+
+	def decrypt_letter(letter)
+		lowercase_letter = letter.downcase
+		cipher.key(lowercase_letter)
+
+	end
+
+	def decrypt(string)
+		results = [ ]
+		split_string = string.slice(7)
+		split_string = split_string.join
+	
+		for x in split_string do
+			decrypted_letter = decrypt_letter(x)
+			results.push(decrypted_letter)
+		end
+		results.join
 	end
 end
