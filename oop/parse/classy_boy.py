60de207ddef8fe8f5fb6a400ef73b034263cf2bd
Kyle <kyle1james@gmail.com>
2018-04-09 10:32:03 -0400

    parsey class

--- oop/parse/classy_boy.py
+++ oop/parse/classy_boy.py
@@ -1,8 +1,9 @@
 import bs4, requests
-
+# beauitful soup for parse
 class Parsey(object):
 
 	def __init__(self, url, find_me):
+		# object as url and css selector
 		self.url = url
 		self.find_me = find_me
 
@@ -13,14 +14,24 @@ class Parsey(object):
 		print('going to %s'%(self.url))
 		# Find requested data
 		soup = bs4.BeautifulSoup(res.text, 'html.parser')
-		#print(soup.get_text())
-		print(soup.find_all(self.find_me))
-		#for link in soup.find_all('a'):
+		# use css selector to locate
+		elems = soup.select(self.find_me)
+		print(elems[0].text)
+		print('')
+
+	# Example of mass loop
+	  #	for link in soup.find_all('a'):
 		#	print(link.get('href'))
+
 		
 
-
-dogs = Parsey('https://www.dwight.edu/', 'a')
-Parsey.get_url(dogs)
+# Create objects
+jobs = Parsey('https://www.dwight.edu/about/employment-opportunities', '#fsEl_5489 > div > ul')
+nytimes = Parsey('https://www.nytimes.com/', '#topnews-100000005840370 > h2 > a')
+wpost = Parsey('https://www.washingtonpost.com/', '#fKa8822e4tpJOq > div > div > div.headline.small.normal-style.text-align-inherit')
+# Call
+Parsey.get_url(jobs)
+Parsey.get_url(nytimes)
+Parsey.get_url(wpost)
 
 
